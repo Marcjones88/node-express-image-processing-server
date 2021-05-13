5cb87a968520404e285a67098dea9238e89cfac4
Marc Jones <marcjones88@mac.com>
2021-05-13 09:34:33 +0100

    Completed module

--- api/app.js
+++ api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+
 const app = express();
 
 const pathToIndex = path.resolve(__dirname, '../client/index.html');
@@ -8,4 +9,4 @@ app.use('/*', (request, response) =>  {
     response.sendFile(pathToIndex);
 });
 
-module.exports.app;
+module.exports = app;
