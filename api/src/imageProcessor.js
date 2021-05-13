07505f29f1f8a59934c424760370d51a6871635e
Marc Jones <marcjones88@mac.com>
2021-05-13 11:22:33 +0100

    Completed module

--- api/src/imageProcessor.js
+++ api/src/imageProcessor.js
@@ -4,10 +4,14 @@ const { Worker, isMainThread } = require('worker_threads');
 const pathToResizeWorker = path.resolve(__dirname, 'resizeWorker.js');
 const pathToMonochromeWorker = path.resolve(__dirname, 'monochromeWorker.js');
 
+const uploadPathResolver = (filename) => {
+    return path.resolve(__dirname, '../uploads', filename);
+};
+
 const imageProcessor = (filename) =>{
     const sourcePath = uploadPathResolver(filename);
-    const resizedDestination = uploadPathResolver('resized' + filename);
-    const monochromeDestination = uploadPathResolver('monochrome' + filename);
+    const resizedDestination = uploadPathResolver('resized-' + filename);
+    const monochromeDestination = uploadPathResolver('monochrome-' + filename);
     let resizeWorkerFinished = false;
     let monochromeWorkerFinished = false;
     return new Promise((resolve, reject) => {
@@ -26,6 +30,7 @@ const imageProcessor = (filename) =>{
                     if(code !== 0) reject(new Error('Exited with status code ' + code));
                 });
                 monochromeWorker.on('message', (message) => {
+                    monochromeWorkerFinished = true;
                     if(resizeWorkerFinished) resolve('monochromeWorker finished processing');
                 });
                 monochromeWorker.on('error', (error) => {
@@ -35,16 +40,12 @@ const imageProcessor = (filename) =>{
                     if(code !== 0) reject(new Error('Exited with status code ' + code));
                 });
             } catch (error) {
-                reject(error)    ;
+                reject(error);
             }
         }else{
             reject(new Error('not on main thread'));
         };
     });
-};
-
-const uploadPathResolver = (filename) => {
-    path.resolve(__dirname, '../uploads', filename);
 };
 
 module.exports = imageProcessor;
